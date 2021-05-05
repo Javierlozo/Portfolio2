18f43a3e68316cf0eb4fa555c3185b61c668df63
Luis Javier Lozoya <luisloart@gmail.com>
2021-05-04 21:29:42 -0400

    Added Modal1 Project Name

--- src/components/porfolio/Portfolio.js
+++ src/components/porfolio/Portfolio.js
@@ -46,16 +46,12 @@ export default class Portfolio extends Component {
         
             <div id="modal-01" hidden>
                 <div className="modal-popup">
-                    <img src={Modal1_1} alt="" />
+                    <img src="images/portfolio/gallery/g-droplet.jpg" alt="" />
                     <div className="modal-popup__desc">
-                        <h5>Droplet</h5>
+                        <h5>Rental App</h5>
                         <p>Odio soluta enim quos sit asperiores rerum rerum repudiandae cum. Vel voluptatem alias qui assumenda iure et expedita voluptatem. Ratione officiis quae.</p>
-                        <ul className="modal-popup__cat">
-                            <li>Branding</li>
-                            <li>Product Design</li>
-                        </ul>
                     </div>
-                    <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
+                    <a href="https://javierlozo.github.io/Rental-App/" className="modal-popup__details">Project link</a>
                 </div>
             </div> {/* end modal */}
 
@@ -65,9 +61,6 @@ export default class Portfolio extends Component {
                     <div className="modal-popup__desc">
                         <h5>The Lamp</h5>
                         <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                        <ul className="modal-popup__cat">
-                            <li>Branding</li>
-                        </ul>
                     </div>
                     <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                 </div>
@@ -79,9 +72,6 @@ export default class Portfolio extends Component {
                     <div className="modal-popup__desc">
                         <h5>Minimalismo</h5>
                         <p>Exercitationem reprehenderit quod explicabo consequatur aliquid ut magni ut. Deleniti quo corrupti illum velit eveniet ratione necessitatibus ipsam mollitia.</p>
-                        <ul className="modal-popup__cat">
-                            <li>Product Design</li>
-                        </ul>
                     </div>
                     <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                 </div>
@@ -93,9 +83,6 @@ export default class Portfolio extends Component {
                     <div className="modal-popup__desc">
                         <h5>Shutterbug</h5>
                         <p>Dolores velit qui quos nobis. Aliquam delectus voluptas quos possimus non voluptatem voluptas voluptas. Est doloribus eligendi porro doloribus voluptatum.</p>
-                        <ul className="modal-popup__cat">
-                        <li>E-Commerce</li>
-                        </ul>
                     </div>
                     <a href="https://www.behance.net/" className="modal-popup__details">Project link</a>
                 </div>
